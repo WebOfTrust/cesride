98703d1e9c9c0ff5a45cf7b79d20fbca3306aebb
Jason Colburne <j.colburne@gmail.com>
2023-02-21 11:41:56 -0400

    sadder (#91)

--- src/core/common.rs
+++ src/core/common.rs
@@ -22,7 +22,15 @@ pub(crate) struct DeversifyResult {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub(crate) struct Version {
+pub(crate) struct SniffResult {
+    pub(crate) ident: String,
+    pub(crate) kind: String,
+    pub(crate) version: Version,
+    pub(crate) size: u32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Version {
     pub(crate) major: u8,
     pub(crate) minor: u8,
 }
@@ -54,7 +62,11 @@ const REVER_STRING: &str = "(?P<ident>[A-Z]{4})(?P<major>[0-9a-f])(?P<minor>[0-9
 const IDENTS: &[&str] = &[Identage::ACDC, Identage::KERI];
 const SERIALS: &[&str] = &[Serialage::JSON];
 
-const CURRENT_VERSION: &Version = &Version { major: 1, minor: 0 };
+pub const CURRENT_VERSION: &Version = &Version { major: 1, minor: 0 };
+
+const MAXIMUM_START_SIZE: usize = 12;
+const VERSION_FULL_SIZE: usize = 17;
+const MINIMUM_SNIFF_SIZE: usize = MAXIMUM_START_SIZE + VERSION_FULL_SIZE;
 
 pub(crate) fn deversify(vs: &str) -> Result<DeversifyResult> {
     lazy_static! {
@@ -109,19 +121,19 @@ pub(crate) fn sizeify(ked: &Value, kind: Option<&str>) -> Result<SizeifyResult> 
     let size = raw.len();
 
     let start = match REVER.shortest_match(&String::from_utf8(raw.clone())?) {
-        Some(m) => m - 17,
+        Some(m) => m - VERSION_FULL_SIZE,
         // unreachable - deversify has been called which ensures this will match
         None => return err!(Error::Value(format!("invalid version string in raw = {raw:?}"))),
     };
 
-    if start > 12 {
+    if start > MAXIMUM_START_SIZE {
         return err!(Error::Value(format!(
             "invalid version string in raw = {raw:?} start = {start}"
         )));
     }
 
     let fore = raw[..start].to_vec();
-    let mut back = raw[start + 17..].to_vec();
+    let mut back = raw[start + VERSION_FULL_SIZE..].to_vec();
     let vs = versify(Some(&result.ident), Some(&result.version), Some(&kind), Some(size as u32))?;
 
     let mut raw = fore;
@@ -165,6 +177,29 @@ pub(crate) fn versify(
     ))
 }
 
+pub(crate) fn loads(raw: &[u8], size: Option<u32>, kind: Option<&str>) -> Result<Value> {
+    let kind = kind.unwrap_or(Serialage::JSON);
+
+    if let Some(size) = size {
+        match kind {
+            Serialage::JSON => {
+                let v: serde_json::Value =
+                    serde_json::from_str(&String::from_utf8(raw[..(size as usize)].to_vec())?)?;
+                Ok(Value::from(&v))
+            }
+            _ => err!(Error::Validation(format!("invalid serialization kind {kind}"))),
+        }
+    } else {
+        match kind {
+            Serialage::JSON => {
+                let v: serde_json::Value = serde_json::from_str(&String::from_utf8(raw.to_vec())?)?;
+                Ok(Value::from(&v))
+            }
+            _ => err!(Error::Validation(format!("invalid serialization kind {kind}"))),
+        }
+    }
+}
+
 pub(crate) fn dumps(ked: &Value, kind: Option<&str>) -> Result<Vec<u8>> {
     let kind = kind.unwrap_or(Serialage::JSON);
     match kind {
@@ -173,11 +208,82 @@ pub(crate) fn dumps(ked: &Value, kind: Option<&str>) -> Result<Vec<u8>> {
     }
 }
 
+pub(crate) fn sniff(raw: &[u8]) -> Result<SniffResult> {
+    lazy_static! {
+        static ref REVER: Regex = Regex::new(REVER_STRING).unwrap();
+    };
+
+    if raw.len() < MINIMUM_SNIFF_SIZE {
+        return err!(Error::Value(format!(
+            "need more bytes than {bytes} to sniff",
+            bytes = raw.len()
+        )));
+    }
+
+    let raw = &String::from_utf8(raw.to_vec())?;
+    let start = match REVER.shortest_match(raw) {
+        Some(m) => m - VERSION_FULL_SIZE,
+        None => return err!(Error::Value(format!("invalid version string in raw = {raw:?}"))),
+    };
+
+    if start > MAXIMUM_START_SIZE {
+        return err!(Error::Value(format!(
+            "invalid version string in raw = {raw:?} start = {start}"
+        )));
+    }
+
+    let vs = &raw[start..(start + VERSION_FULL_SIZE)];
+
+    let ident = REVER.replace_all(vs, "$ident").to_string();
+    let major = u8::from_str_radix(&REVER.replace_all(vs, "$major"), 16)?;
+    let minor = u8::from_str_radix(&REVER.replace_all(vs, "$minor"), 16)?;
+    let kind = REVER.replace_all(vs, "$kind").to_string();
+    let size = u32::from_str_radix(&REVER.replace_all(vs, "$size"), 16)?;
+    let version = Version { major, minor };
+
+    if !SERIALS.contains(&kind.as_str()) {
+        return err!(Error::Validation(format!("invalid serialization kind {kind}")));
+    }
+
+    Ok(SniffResult { ident, kind, version, size })
+}
+
 #[cfg(test)]
 mod test {
     use crate::core::common;
-    use crate::data::data;
+    use crate::data::{data, Data};
     use rstest::rstest;
+
+    #[test]
+    fn loads() {
+        let raw = &data!({}).to_json().unwrap().as_bytes().to_vec();
+        assert!(common::loads(raw, None, None).is_ok());
+    }
+
+    #[test]
+    fn sniff_sad_paths() {
+        assert!(common::sniff(&[]).is_err()); // minimum 29 octets
+        assert!(common::sniff(
+            &data!({"v":"version string must be valid!"}).to_json().unwrap().as_bytes()
+        )
+        .is_err());
+        assert!(common::sniff(
+            &data!({"i":"needs to start within 12 characters!","v":"KERI10JSON000000_"})
+                .to_json()
+                .unwrap()
+                .as_bytes()
+        )
+        .is_err());
+        assert!(common::sniff(&data!({"v":"KERI10ABCD000000_","confusing but necessary filler":"hmm...maybe a 12 octet magic prefix?"}).to_json().unwrap().as_bytes()).is_err());
+        // needs to have a valid serialization kind
+    }
+
+    #[test]
+    fn loads_sad_paths() {
+        let raw = &data!({}).to_json().unwrap().as_bytes().to_vec();
+        assert!(common::loads(raw, None, Some("CESR")).is_err());
+        assert!(common::loads(raw, Some(1024), Some("CESR")).is_err());
+    }
 
     #[test]
     fn sizeify_sad_paths() {
