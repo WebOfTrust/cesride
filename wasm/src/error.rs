ae91fcf8c6b12bafbf166165087af51def325968
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2023-03-17 14:06:41 +0300

    Cleanup error.rs

--- wasm/src/error.rs
+++ wasm/src/error.rs
@@ -1,45 +1,14 @@
-use cesride_core::Error as CesrError;
 use cesride_core::Result as CesrResult;
-use js_sys::{Error as JsError, JsString};
-use wasm_bindgen::{prelude::*, JsCast};
+use js_sys::Error as JsError;
 
 // Allows conversion of CesrError to javascript error
 pub(crate) trait JsResult<T> {
+    #[allow(clippy::wrong_self_convention)]
     fn as_js(self) -> Result<T, JsError>;
 }
 
 impl<T> JsResult<T> for CesrResult<T> {
     fn as_js(self) -> Result<T, JsError> {
         self.map_err(|e| JsError::new(&e.to_string()))
-    }
-}
-
-// Allows conversion of javascript error to CesrError
-pub(crate) trait FromJsResult<T> {
-    fn from_js(self) -> CesrResult<T>;
-}
-
-impl<T> FromJsResult<T> for Result<T, JsValue> {
-    fn from_js(self) -> CesrResult<T> {
-        self.map_err(|e| {
-            // String was thrown
-            if let Some(e) = e.dyn_ref::<JsString>() {
-                let msg = e.to_string();
-                return CesrError::Conversion(msg.into()).into();
-            }
-
-            // Error instance was thrown
-            if let Some(e) = e.dyn_ref::<JsError>() {
-                let message = e.message().as_string().unwrap_or(format!("{:?}", e));
-                return match e.name().as_string().as_deref() {
-                    Some("CesrErrorCommon") => CesrError::InvalidVarSize(message),
-                    Some(msg) => CesrError::Conversion(msg.to_string()),
-                    _ => CesrError::Conversion("Unknown error".to_string()),
-                }
-                .into();
-            }
-
-            CesrError::Conversion("Unknown error".to_string()).into()
-        })
     }
 }
